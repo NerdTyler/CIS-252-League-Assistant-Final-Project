a1c3032a93b220fd04e4f59b617b2f932b2c04d2
Tyler B <tyler3686@gmail.com>
2018-04-25 16:26:34 -0400

    Removed example links

--- demo_stats.js
+++ demo_stats.js
@@ -4,5 +4,3 @@ rawteamstats[1] = "Jets,2,4,1";
 rawteamstats[2] = "AppleDumplings,3,3,1";
 rawteamstats[3] = "MightyShrimp, 1, 5, 0";
 rawteamstats[4] = "Volcano, 2, 2, 2";
-rawteamstats[5] = "Ten, 10,0,0";
-//array.split based on commas
